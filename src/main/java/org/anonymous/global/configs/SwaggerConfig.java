122437ba8cd4bbd2448f1caf2f9168dbe470969e
kks940104 <kks940104@gmail.com>
2025-01-17 17:08:15 +0900

    card 초기세팅 - kks
    card 초기세팅 - kks

---
+++ src/main/java/org/anonymous/global/configs/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package org.anonymous.global.configs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(info=@Info(title="Card Service API"))
+public class SwaggerConfig {
+    
+    @Bean
+    public GroupedOpenApi apiGroup() {
+        return GroupedOpenApi.builder()
+                .group("Card Service API v1")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
