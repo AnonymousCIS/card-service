72e8ccc997dcb7411b14a0fa3da37695cfcf40b2
kks940104 <kks940104@gmail.com>
2025-02-20 02:14:52 +0900

    카드 스케쥴러 추가

--- src/main/java/org/anonymous/CardServiceApplication.java
+++ src/main/java/org/anonymous/CardServiceApplication.java
@@ -5,9 +5,11 @@ import jakarta.annotation.PreDestroy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class CardServiceApplication {
+@EnableScheduling
+class CardServiceApplication {
 
 	@Autowired
 	private EurekaClient eurekaClient;
